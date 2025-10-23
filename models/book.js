9e067e4c9872bbe9bc303000a797e6dd91351a8c
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2025-10-23 14:34:20 +0200

    Create the Book Model
    Create the new Book model in the models/book.js file and ensure it has the following properties set to the correct data types:
    title - string
    author - string
    genre - string
    year - integer
    Add the following settings to the title and author properties:
    - Set the allowNull property to false
    - Add Sequelize validation and appropriate error messages to ensure the values cannot be empty when creating or updating a book in the database

---
+++ models/book.js
@@ -0,0 +1,58 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Book extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Book.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Title must not be empty"
+        },
+        notNull: {
+          msg: "Title is required"
+        },
+      },
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Author must not be empty"
+        },
+        notNull: {
+          msg: "Author is required"
+        },
+      },
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        isInt: {
+          msg: "Year must be an integer"
+        },
+    },
+  }, {
+    sequelize,
+    modelName: 'Book',
+  });
+  return Book;
+};
