463c0410ef1426d9de348ef53b6aa994902994b8
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2025-10-25 10:30:27 +0200

    Home Route
    Route: get / - Should redirect to the /books route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Redirect home route to /books
+router.get ('/', (req, res) => {
+  res.redirect('/books');
 });
 
 module.exports = router;
